9343741957629a427f3ca7a0df44e6cea350ce89
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-06 20:02:10 +0100

    added: Project.cs

---
+++ Prioriza.Web/Data/Entities/Project.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Prioriza.Web.Data.Entities;
+
+[Table("Projects")]
+public class Project
+{
+    [Key] public int Id { get; set; }
+    [MaxLength(100)] public required string Name { get; set; }
+    [MaxLength(200)] public string Description { get; set; } = String.Empty;
+    [MaxLength(450)] public required string UserId { get; set; }
+    public ApplicationUser User { get; set; } = null!;
+    public required ICollection<WorkItem> Tasks { get; set; } = new List<WorkItem>();
+}
