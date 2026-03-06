b44f91d3c0e6cfff0a213d79051fdb46f7ef82df
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-06 20:02:22 +0100

    added WorkItem.cs

---
+++ Prioriza.Web/Data/Entities/WorkItem.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prioriza.Web.Data.Entities;
+
+[Table("WorkItems")]
+public class WorkItem
+{
+    [Key] 
+    public int Id { get; set; }
+
+    [MaxLength(200)] 
+    public required string Title { get; set; }
+    [MaxLength(200)] 
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsCompleted { get; set; } = false;
+
+    public int? ProjectId { get; set; }
+
+    public Project? Project { get; set; }
+
+    public required string UserId { get; set; }
+
+    public ApplicationUser User { get; set; } = null!;
+}
