468cbbb7d17a1465a402f21122643a73c73ec618
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-08 23:09:11 +0100

    feat: add IsInbox to Project.cs

--- Prioriza.Web/Data/Entities/Project.cs
+++ Prioriza.Web/Data/Entities/Project.cs
@@ -11,6 +11,7 @@ public class Project
     [MaxLength(100)] public required string Name { get; set; }
     [MaxLength(200)] public string Description { get; set; } = String.Empty;
     [MaxLength(450)] public required string UserId { get; set; }
+    public bool IsInbox { get; set; } = false;
     public ApplicationUser User { get; set; } = null!;
     public ICollection<WorkItem> Tasks { get; set; } = new List<WorkItem>();
 }
