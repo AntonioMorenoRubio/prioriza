126a7a9aa55033e8a803b5495ab0aaaf258de0a3
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-21 16:09:52 +0100

    feat: translated SetPassword to spanish

--- Prioriza.Web/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
+++ Prioriza.Web/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -50,9 +50,9 @@ namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Nueva contraseña")]
             public string NewPassword { get; set; }
 
             /// <summary>
@@ -60,8 +60,8 @@ namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "Confirmar nueva contraseña")]
+            [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no coinciden.")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -107,7 +107,7 @@ namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your password has been set.";
+            StatusMessage = "Tu contraseña ha sido establecida correctamente.";
 
             return RedirectToPage();
         }
