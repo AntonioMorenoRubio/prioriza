41089f4c74cf37dc90230d14129fa713b4733c32
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-21 19:57:22 +0100

    feat: translated Manage/ExternalLogins to spanish

--- Prioriza.Web/Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
+++ Prioriza.Web/Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
@@ -90,12 +90,13 @@ namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
             var result = await _userManager.RemoveLoginAsync(user, loginProvider, providerKey);
             if (!result.Succeeded)
             {
-                StatusMessage = "The external login was not removed.";
+                
+                StatusMessage = "El inicio de sesión externo no se eliminó.";
                 return RedirectToPage();
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "The external login was removed.";
+            StatusMessage = "El inicio de sesión externo fue eliminado.";
             return RedirectToPage();
         }
 
@@ -128,14 +129,14 @@ namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
             var result = await _userManager.AddLoginAsync(user, info);
             if (!result.Succeeded)
             {
-                StatusMessage = "The external login was not added. External logins can only be associated with one account.";
+                StatusMessage = "El inicio de sesión externo no se añadió. Los inicios de sesión externos solo pueden asociarse con una cuenta.";
                 return RedirectToPage();
             }
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-            StatusMessage = "The external login was added.";
+            StatusMessage = "El inicio de sesión externo fue añadido.";
             return RedirectToPage();
         }
     }
