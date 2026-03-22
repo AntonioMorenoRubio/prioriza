0645376b661f8dd2221bbc08f340217b128171b4
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-03-22 17:22:43 +0100

    refactor: optimized imports for Prioriza.Web

--- Prioriza.Web/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ Prioriza.Web/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -1,11 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-using System;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Prioriza.Web.Data.Entities;
 
 namespace Prioriza.Web.Areas.Identity.Pages.Account.Manage
